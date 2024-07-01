2b758aed58945aa73807d89fd9edef893dc20d64
dicksonma <dickson.ma@claritas.asia>
2024-07-02 01:36:00 +0800

    update agenda

--- src/app/lib/data/agenda.js
+++ src/app/lib/data/agenda.js
@@ -1,12 +1,15 @@
 export default [
   {
-    time: "7:00pm",
-    agenda: "Arrival & Registration",
-    description: ["Refreshments Served", "Guests to be seated at their tables"],
+    time: "5:30pm",
+    agenda: "ARRIVAL & REGISTRATION",
+    description: [
+      "Refreshments Served",
+      "Guests to be seated at their tables",
+    ],
   },
   {
-    time: "7:30pm",
-    agenda: "Opening Remarks",
+    time: "6:30pm",
+    agenda: "OPENING REMARKS",
     description: [
       "Recap video of Motherhood Choice Award 2023",
       "Emcee Welcome Remarks",
@@ -16,68 +19,78 @@ export default [
     ],
   },
   {
-    time: "7:35pm",
-    agenda: "Award Ceremony Commencement",
-    description: ["Main Categories introduction", "Award Ceremony Part 1"],
+    time: "6:35pm",
+    agenda: "AWARD CEREMONY COMMENCEMENT",
+    description: [
+      "Main Categories introduction",
+      "Award Ceremony Segment 1 - Family Category",
+    ],
+  },
+  {
+    time: "6:45pm",
+    agenda: "AWARD CEREMONY SEGMENT 2 - KIDS CATEGORY",
+    description: ["Honoring excellence in the Kids Category"],
+  },
+  {
+    time: "6:55pm",
+    agenda: "INTERMISSION 1: QUIZ CONTEST & BEST DRESSED CONTEST",
+    description: ["Enjoy a fun quiz and celebrate the best dressed! Test your knowledge and showcase your style. Exciting prizes await!"],
+  },
+  {
+    time: "7:00pm",
+    agenda: "DINNER"
+  },
+  {
+    time: "7:10pm",
+    agenda: "AWARD CEREMONY SEGMENT 3 - MOTHERS CATEGORY",
+    description: ["Celebrate outstanding achievements in products and services supporting Mothers on their journey."],
+  },
+  {
+    time: "7:20pm",
+    agenda: "AWARD CEREMONY SEGMENT 4 - BABY CATEGORY",
+    description: ["Celebrate the best in baby products and services."],
+  },
+  {
+    time: "7:30pm",
+    agenda: "INTERMISSION 2: PERFORMANCE BY GOGOKIDS",
+    description: ["Enjoy an exciting performance by GoGoKids! Sit back and be entertained by their vibrant showcase."],
   },
   {
     time: "7:50pm",
-    agenda: "Expert Awards",
-    description: ["Introduction to Motherhood's Experts"],
+    agenda: "INTERMISSION 3: QUIZ CONTEST & BEST DRESSED CONTEST CLOSING"
   },
   {
     time: "8:00pm",
-    agenda: "Dinner & Performance",
-    description: ["Dinner Commence", "Performance by Gogokids"],
+    agenda: "MOTHERHOOD EXPERTS AWARD",
+    description: ["Recognizing our doctors, nurses and panel of experts that shapes motherhood."],
+  },
+  {
+    time: "8:05pm",
+    agenda: "MOTHERHOOD SUPERMOM AWARD",
+    description:["Honoring extraordinary supermoms who inspire and lead by example."]
   },
   {
     time: "8:20pm",
-    agenda: "Award Ceremony Part 2",
+    agenda: "INTERMISSION 4: QUIZ WINNER ANNOUNCEMENT & BEST DRESSED ANNOUNCEMENT (MAN & WOMAN)",
+    description:["Find out the quiz champions and who shines as Best Dressed (Man & Woman)!"]
   },
   {
     time: "8:35pm",
-    agenda: "KOL Supermom Awards",
-    description: ["Introduction to Motherhood's Supermom"],
-  },
-  {
-    time: "8:40pm",
-    agenda: "Motherhood Quiz Contest",
-    description: [
-      "Test your knowledge on allthings motherhood and parenting, and win RM100.",
-    ],
-  },
-  {
-    time: "",
-    agenda: "Best Dressed Contest",
-    description: [
-      "Join us as we celebrate style and elegance with our Best Dressed Contest. Emcee will announce the nominees for Best Dressed Man and Woman, recognizing those who stood out with their fashion choices.",
-    ],
+    agenda: "MOTHERHOOD SUPER PARENTS AWARD",
+    description: ["Recognizing outstanding parents whose love and dedication set a remarkable example in nurturing their children."],
   },
   {
     time: "8:45pm",
-    agenda: "Award Ceremony Part 3",
+    agenda: "INTERMISSION 5: PERFORMANCE BY HUNNY MADU",
+    description:["Experience the captivating performance by Hunny Madu!"]
+  },
+  {
+    time: "8:50pm",
+    agenda: "CLOSING REMARKS & PHOTO SESSION",
+    description: ["Conclude the evening with inspiring closing remarks and capture memories with a group photo session."],
   },
   {
     time: "9:00pm",
-    agenda: "Celebrity Supermom Award",
-    description: ["Introduction to Motherhood's Celebrity"],
-  },
-  {
-    time: "9:15pm",
-    agenda: "Award Ceremony Part 4",
-  },
-  {
-    time: "9:30pm",
-    agenda:
-      "Quiz Winner Announcement and Best Dressed Winner Announcement (Man & Woman)",
-  },
-  {
-    time: "9:40pm",
-    agenda: "Motherhood Super Parents Award",
-    description: ["Introduction to Motherhood's CSuper Parents"],
-  },
-  {
-    time: "9:55pm",
-    agenda: "Closing Session",
-  },
+    agenda: "EVENT END"
+  }
 ];
